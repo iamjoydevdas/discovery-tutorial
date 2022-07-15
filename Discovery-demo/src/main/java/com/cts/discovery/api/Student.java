93179df0bb64b3092514ccdc9bddcc1a481b3c4a
Joydev Das <iamjoydevdas@gmail.com>
2022-07-15 10:54:35 +0530

    Story no - Init commit

---
+++ Discovery-demo/src/main/java/com/cts/discovery/api/Student.java
@@ -0,0 +1,16 @@
+package com.cts.discovery.api;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Student  {
+    private Integer id;
+    private String name;
+    private Integer age;
+    private String address;
+}

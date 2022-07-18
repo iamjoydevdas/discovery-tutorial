2f1c168338301b3b0ed5e447d2d81b7226d0e96e
Joydev Das <iamjoydevdas@gmail.com>
2022-07-18 10:02:00 +0530

    open api

--- Discovery-demo/DemoMod1/src/main/java/discovery/DiscoveryDemo.java
+++ Discovery-demo/DemoMod1/src/main/java/discovery/DiscoveryDemo.java
@@ -1,9 +1,12 @@
 package discovery;
 
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(title = "User API", version = "1.0", description = "User Management Api"))
 @SpringBootApplication
 public class DiscoveryDemo {
     public static void main(String[] args) {

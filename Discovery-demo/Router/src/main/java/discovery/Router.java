460ad3308eb4825fd772a7c3bb570a2846f45229
Joydev Das <iamjoydevdas@gmail.com>
2022-07-20 08:53:10 +0530

    router

---
+++ Discovery-demo/Router/src/main/java/discovery/Router.java
@@ -0,0 +1,17 @@
+package discovery;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class Router {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Router.class, args);
+    }
+}

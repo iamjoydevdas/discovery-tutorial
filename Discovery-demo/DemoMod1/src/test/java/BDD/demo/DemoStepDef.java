93092ba3daecf1c11169c2130656fc096dff8964
Joydev Das <iamjoydevdas@gmail.com>
2022-07-26 11:31:12 +0530

    cucumber and vault

--- Discovery-demo/DemoMod1/src/test/java/BDD/demo/DemoStepDef.java
+++ Discovery-demo/DemoMod1/src/test/java/BDD/demo/DemoStepDef.java
@@ -1,46 +1,38 @@
 package BDD.demo;
 
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.junit.Assert;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
 
 @CucumberContextConfiguration
 public class DemoStepDef {
 
     RestTemplate restTemplate = new RestTemplate();
-    ObjectMapper mapper = new ObjectMapper();
-
-    String pingOutput;
-
+    String responseRes;
     @Given("^The application is running$")
     public void the_application_is_running() throws Throwable {
-        final ResponseEntity<Object> response = restTemplate.getForEntity("http://localhost:8080/actuator/health", Object.class);
+        final ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:8080/actuator/health", String.class);
         Assert.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 
 
     @When("^the user hit /ping api$")
     public void the_user_hit_ping_api() throws Throwable {
-
-        final ResponseEntity<String> exchange = restTemplate.exchange("http://localhost:8080/ping", HttpMethod.GET, null, String.class);
-        pingOutput = exchange.getBody();
+        final ResponseEntity<String> res = restTemplate.getForEntity("http://localhost:8080/ping", String.class);
+        responseRes = res.getBody();
 
     }
 
     @Then("^he will get the result \"([^\"]*)\"$")
     public void he_will_get_the_result(String arg1) throws Throwable {
-        Assert.assertEquals(pingOutput, arg1);
+        Assert.assertEquals(arg1, responseRes);
+
     }
 }
 
--- Discovery-demo/DemoMod1/src/test/resources/DemoApi.feature
+++ Discovery-demo/DemoMod1/src/test/resources/DemoApi.feature
@@ -5,7 +5,7 @@ Feature: Demo Api Behaviour
     When the user hit /ping api
     Then he will get the result "We are learning Spring Boot"
 
-  Scenario: User wants to hit ping API s
+  Scenario: User wants to hit ping API one more time
     Given The application is running
     When the user hit /ping api
     Then he will get the result "We are learning Spring Boot"

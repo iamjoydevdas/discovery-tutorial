41a600c5bbdc6b1d4f2b96835ea2c0993e4f4330
Joydev Das <iamjoydevdas@gmail.com>
2022-07-28 09:53:41 +0530

    jwt

---
+++ Discovery-demo/JWTDemo/src/main/java/discovery/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package discovery.repository;
+
+import discovery.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Integer> {
+    User findByUserName(String username);
+}
